3fb59c8d021a033f5069b66812d2774ab38c4e35
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-26 20:12:38 -0700

    Add express_server as server connection file

---
+++ express_server.js
@@ -0,0 +1,24 @@
+// Import the express module from the library
+const express = require("express");
+// Create an instance of Express
+const app = express();
+// Define the port number for the server to listen on (8080 default port)
+const PORT = 8080;
+
+// A database storing shortened URLs as key-value pairs
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// Route handler for the root endpoint
+app.get("/", (req, res) => {
+  // Send "Hello!" as the response when a GET request is made to the root endpoint
+  res.send("Hello!");
+});
+
+// Start the server and listen for incoming requests on the specified port
+app.listen(PORT, () => {
+  // Log a message indicatign that the server is running and listening on the specified port
+  console.log(`Example app listening on port: ${PORT}`);
+});
