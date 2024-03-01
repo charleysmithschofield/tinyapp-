9738f2f8495d6bb14acfb633f4590c288ba864e3
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-29 21:27:23 -0700

    add route handler for /urls to handle POST requests

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 // Import the express module from the library
 const express = require("express");
+
 // Create an instance of Express
 const app = express();
+
 // Define the port number for the server to listen on (8080 default port)
 const PORT = 8080;
 
@@ -10,7 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Sets view engine to ejs
 app.set("view engine", "ejs");
-
 
 // A database storing shortened URLs as key-value pairs
 const urlDatabase = {
@@ -23,6 +24,7 @@ app.get("/", (req, res) => {
   // Send "Hello!" as the response when a GET request is made to the root endpoint
   res.send("Hello!");
 });
+
 // Route handler for the "/urls.json" endpoint
 app.get("/urls.json", (req, res) => {
   // Send the urlDatabase object as a JSON response when a GET request is made to the "/urls.json" endpoint
@@ -31,32 +33,41 @@ app.get("/urls.json", (req, res) => {
 
 // Route handler for the "/hello" endpoint
 app.get("/hello", (req, res) => {
-  // Send the HTML resoibse with a greeting to the world
+  // Send the HTML response with a greeting to the world
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 // Route handler for the "/urls" endpoint
 app.get("/urls", (req, res) => {
+  // Provide the urlDatabase to the urls_index template
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 // Route handler for URLs new page
 app.get("/urls/new", (req, res) => {
+  // Render the urls_new template for creating a new shortened URL
   res.render("urls_new");
 });
 
-// Route handler for the "urls/:id"
+// Route handler for the "/urls/:id" endpoint
 app.get("/urls/:id", (req, res) => {
   // Fetch the long URL from the urlDatabase by providing the corresponding id
   const longURL = urlDatabase[req.params.id];
+  // Provide the id and longURL to the urls_show template
   const templateVars = { id: req.params.id, longURL: longURL };
   res.render("urls_show", templateVars);
+});
+
+// Route handler for the "/urls" endpoint to handle POST requests
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 
 // Start the server and listen for incoming requests on the specified port
 app.listen(PORT, () => {
-  // Log a message indicatign that the server is running and listening on the specified port
+  // Log a message indicating that the server is running and listening on the specified port
   console.log(`Example app listening on port: ${PORT}`);
 });
