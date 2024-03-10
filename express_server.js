5380e82b2eed3b2174bb738700662e6f6733dd24
charleysmithschofield <charleysmithschofield@gmail.com>
2024-03-09 20:15:01 -0700

    add key1 and key2 to keys of cookieSession

--- express_server.js
+++ express_server.js
@@ -1,32 +1,24 @@
-// Import the express module from the library
+// Imports
 const express = require("express");
-// Import cookie-parser
-const cookieParser = require("cookie-parser");
-// Import bcrypt
-const bcrypt = require("bcryptjs");
-
-
-// Create an instance of Express
 const app = express();
-// Define the port number for the server to listen on (8080 default port)
 const PORT = 8080;
-
-
-
-// Middle-ware to parse the URL-encoded form data
-app.use(express.urlencoded({ extended: true }));
-// Middle-ware to parse the cookies
-app.use(cookieParser());
-
-
+const cookieSession = require('cookie-session');
+const bcrypt = require("bcryptjs");
 
 // Sets view engine to ejs
 app.set("view engine", "ejs");
 
+// Middle-ware to parse the URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+// Middle-ware to encrypt cookie using cookieSession
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 
 
-// Database to store shortened URLs as key-value pairs
-const urlDatabase = {
+// urlDatabase object to store URL info
+let urlDatabase = {
   b6UTxQ: {
     longURL: "http://www.lighthouselabs.ca",
     userID: "22@22.ca",
@@ -37,7 +29,7 @@ const urlDatabase = {
   },
 };
 
-// Database to store users
+// User object to store users
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -52,10 +44,9 @@ const users = {
 };
 
 
-
-// Function to generate a string of 6 random alphanumeric characters
+// Function to generate a string to create shortURL ID
 const generateRandomString = function() {
-  // Variable called randomString to store the random alphanumeric string
+  // Variable called randomString to store the shortURL ID
   let randomString = '';
   // Variable called characters containing the alphabet in lower and uppercase, as well as, the numbers from 0 to 9
   const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV0123456789';
@@ -66,6 +57,34 @@ const generateRandomString = function() {
   }
   return randomString;
 };
+
+// Function to generate random user ID
+const generateRandomID = function() {
+  // Variable called randomString to store the shortURL ID
+  let randomString = '';
+  // Variable called characters containing the alphabet in lower and uppercase, as well as, the numbers from 0 to 9
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV0123456789';
+  // For loop to iterate the characters
+  for (let i = 0; i < 6; i++) {
+    // add random characters to the randomString. Use .charAt, Math.floor and Math.random to generate random characters
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+};
+
+// Function to get user by email
+const getUserByEmail = function(email) {
+  // For in loop to iterate through the users
+  for (const userId in users) {
+    // Check if the email used matches an email already in the current users
+    if (users[userId].email === email) {
+      return users[userId];
+    }
+  }
+  // If user is not found, return null
+  return null;
+};
+
 
 // Function to retireve user object by user ID
 const getUserById = function(userId) {
@@ -95,7 +114,6 @@ const urlsForUser = function(userID) {
 };
 
 
-
 // GETS HERE -------------------------------------------
 
 // GET route for the root endpoint
@@ -105,13 +123,11 @@ app.get("/", (req, res) => {
 });
 
 
-
 // GET route for /urls.json endpoint
 app.get("/urls.json", (req, res) => {
   // Send the urlDatabase object as a JSON response when a GET request is made to the "/urls.json" endpoint
   res.json(urlDatabase);
 });
-
 
 
 // GET route for /hello endpoint
@@ -121,11 +137,10 @@ app.get("/hello", (req, res) => {
 });
 
 
-
 // GET route for /urls endpoint
 app.get("/urls", (req, res) => {
-  // Retrieve the user object based on the user_id cookie
-  const user = getUserById(req.cookies["user_id"]);
+  // Retrieve the user object based on the user_id session
+  const user = getUserById(req.session.user_id);
 
   // Check if the user is not logged in
   if (!user) {
@@ -135,44 +150,37 @@ app.get("/urls", (req, res) => {
     <p><a href="/login">Log in</a> or <a href="/register">Register</a></>
   `);
   }
-
   // Filter URLs to display only the URLs belonging to the logged-in user
   const userURLs = urlsForUser(user.id);
-
 
   // Create template variable object to pass to he templateVars
   const templateVars = {
     user: user, // Pass the user object to the template
     urls: urlDatabase // Pass the urlDatabase object to the template
   };
-
   // Render the "urls_index" template with the template variables
   res.render("urls_index", templateVars);
 });
 
 
-
 // GET route for /urls/new
 app.get("/urls/new", (req, res) => {
-  // Retrieve the user object based on the user_id cookie
-  const user = getUserById(req.cookies["user_id"]);
+  // Retrieve the user object based on the user_id
+  const user = getUserById(req.session.user_id);
 
   // check if the user is not logged in
   if (!user) {
     // if user is not logged in redirect the user to the /login endpoint
     return res.redirect("/login");
   }
-
   // Provide the urlDatabase to the urls_index template
   const templateVars = {
     user: user, // Pass the user object to the template
     urls: urlDatabase
   };
-
   // Render the urls_new template for creating a new shortened URL
   res.render("urls_new", templateVars);
 });
-
 
 
 // GET route for the "/urls/:id" endpoint
@@ -180,8 +188,8 @@ app.get("/urls/:id", (req, res) => {
   // Fetch the short URL from the request parameters
   const shortURL = req.params.id;
 
-  // Retrieve the user object based on the user_id cookie
-  const user = getUserById(req.cookies["user_id"]);
+  // Retrieve the user object based on the user_id
+  const user = getUserById(req.session.user_id);
 
   // Check if the short URL exists in the urlDatabase
   if (urlDatabase[shortURL]) {
@@ -212,11 +220,10 @@ app.get("/urls/:id", (req, res) => {
 });
 
 
-
 // GET route for the "/register" endpoint
 app.get("/register", (req, res) => {
-  // Define the user object based on the user_id cookie
-  const user = getUserById(req.cookies && req.cookies.user_id);
+  // Define the user object based on the user_id
+  const user = getUserById(req.session.user_id);
 
   // Check if the user is already logged in
   if (user) {
@@ -230,12 +237,11 @@ app.get("/register", (req, res) => {
 });
 
 
-
 // GET route for the "/login" endpoint
 app.get('/login', (req, res) => {
 
   // Check if the user is already logged in
-  const user = getUserById(req.cookies.user_id);
+  const user = getUserById(req.session.user_id);
   if (user) {
     // If user is already logged in, redirect to the /urls page
     return res.redirect("/urls");
@@ -244,21 +250,19 @@ app.get('/login', (req, res) => {
   res.render('login', { user: req.user }); // Assuming user data is available in req.user
 });
 
-
 // POSTS HERE =======================================================
 
 
 // POST route for the /urls endpoint
 app.post("/urls", (req, res) => {
   // Define userID
-  const userID = req.cookies.user_id;
+  const userID = req.session.user_id;
   // Check if the user is logged in
   const user = getUserById(userID);
   if (!user) {
     // If the user is not logged in, display an HTML message stating they must be logged in to shorten URLs
     return res.status(403).send("<h1>You must be logged in to shorten URLs.</h1>");
   }
-
   // Retrieve longURL from the request body
   const longURL = req.body.longURL;
 
@@ -276,27 +280,33 @@ app.post("/urls", (req, res) => {
 
 // POST route to delete a URL
 app.post("/urls/:id/delete", (req, res) => {
+  // Check if the user is logged in
+  const userId = req.session.user_id;
   // Retrieve the URL ID from the request parameters
   const urlId = req.params.id;
 
-  // Check if the user is logged in
-  const userId = req.cookies.user_id;
   if (!userId) {
     // If user is not logged in, send an error message
     return res.status(401).send("Error: You are not authorized to delete this URL.");
   }
+  
+  // Check if the URL ID exists in the database
+  if (!urlDatabase[urlId]) {
+    // If the URL does not exist, send an error message
+    return res.status(404).send("Error: URL not found.");
+  }
 
   // Check if the logged-in user is the owner of the URL
   if (urlDatabase[urlId].userID !== userId) {
-    // If user is now the owner of the URL, send an error message
+    // If user is not the owner of the URL, send an error message
     return res.status(403).send("Error: You are not authorized to delete this URL.");
   }
-
   // Delete the URL from the urlDatabase
   delete urlDatabase[urlId];
   // Redirect back to the urls index page ("/urls")
   res.redirect("/urls");
 });
+
 
 
 
@@ -317,19 +327,17 @@ app.get("/u/:id", (req, res) => {
 
 
 
-
 // POST route to update a URL
 app.post("/urls/:id", (req, res) => {
   // Extract the URL ID from the request parameters
   const urlId = req.params.id;
-  // Get the logged in user ID
-  const userId = req.cookies.user_id;
+  // Get the logged in user ID from session
+  const userId = req.session.user_id;
 
   // Check if the URL ID exists in the urlDatabase
   if (!urlDatabase[urlId]) {
     return res.status(404).send("URL not found");
   }
-
   // Check if the user is logged in
   if (!userId) {
     return res.status(401).send("You must be logged in to update a URL");
@@ -348,6 +356,8 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+
+
 // POST route to /login
 app.post("/login", (req, res) => {
   // Retrieve the email and password from the request body
@@ -361,7 +371,7 @@ app.post("/login", (req, res) => {
     // Check if user exists and if the password matches
     if (user && bcrypt.compareSync(password, user.password)) {
       // Set a cookie named 'user_id' with the user's ID
-      res.cookie('user_id', user.id);
+      req.session.user_id = user.id;
       // Redirect the user back to the /urls page
       res.redirect('/urls');
     } else {
@@ -376,29 +386,13 @@ app.post("/login", (req, res) => {
 });
 
 
-
 // POST route to /logout
 app.post("/logout", (req, res) => {
   // Clear the username cookie
-  res.clearCookie('user_id');
+  req.session.user_id = null;
   // Redirect the user back to the /urls page
   res.redirect('/login');
 });
-
-
-// Helper function to find user by email
-const getUserByEmail = function(email) {
-  // For in loop to iterate through the users
-  for (const userId in users) {
-    // Check if the email used matches an email already in the current users
-    if (users[userId].email === email) {
-      return users[userId];
-    }
-  }
-  // If user is not found, return null
-  return null;
-};
-
 
 
 // POST route for the /register endpoint
@@ -439,7 +433,7 @@ app.post("/register", (req, res) => {
   // Log the new user for testing
   console.log("New user registered:", newUser);
   // Set a user_id cookie containing the user's new generated ID
-  res.cookie('user_id', userId);
+  req.session.user_id = userId;
   
   // Redirect user to the /urls page
   res.redirect("/urls");
